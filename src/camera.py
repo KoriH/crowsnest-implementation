4bff1e84b7e2f303ce5d8265165cb0df78495d9e
Skidlez <96402323+korihuen@users.noreply.github.com>
2024-04-06 13:48:54 -0700

    added stuff

--- src/camera.py
+++ src/camera.py
@@ -17,16 +17,34 @@ boxes = {}
 frames = {}
 times = {}
 
+
+# for collision velocity
+def compute_velocity(tracker_id, centroid_x, centroid_y, scale_factor):
+
+    prev_x, prev_y = centroids.get(tracker_id, (centroid_x, centroid_y))
+    frame_count = frames.get(tracker_id, 1)
+    
+
+    dx = (centroid_x - prev_x) * scale_factor
+    dy = (centroid_y - prev_y) * scale_factor
+    
+
+    velocity = np.sqrt(dx**2 + dy**2) / frame_count * 30
+    velocities[tracker_id] = round(velocity, 1)
+
 # bounding box for models
 def annotate_frame(frame, x1, y1, x2, y2, tracker_id):
+    # Round bounding box coordinates
     x1, x2, y1, y2 = int(x1), int(x2), int(y1), int(y2)
     
+    # Draw bounding box and velocity label
     frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
     frame = cv2.putText(frame, f"{velocities[tracker_id]}", (x2+7, y2), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 1)
     return frame
 
 # red frame for collision
 def collision_frame(frame, tracker_id, other_tracker_id):
+    # Threshold for collision detection (Major)
     if velocities[tracker_id] > 10 or velocities[other_tracker_id] > 10:
         box1 = boxes[tracker_id]
         box2 = boxes[other_tracker_id]
@@ -60,7 +78,11 @@ while cap.isOpened():
         frame_id += 1
         uncropped = frame.copy()
         frame = frame[220:530, :]
+        
+        # Perform inference on the frame
         results = model(frame)[0]
+        
+        # Format detections object
         detections = sv.Detections.from_ultralytics(results)
         
         detections = tracker.update_with_detections(detections)
@@ -83,9 +105,11 @@ while cap.isOpened():
                     frames[tracker_id] = frame_id
             
 
+        # Display the annotated frame
         if annotated_frame is not None:
             cv2.imshow("YOLOv8 Inference", annotated_frame)
         
+        # Break the loop if 'q' is pressed
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
     else:
