0f02bdb49d3a21ab06236f18c8b3c975da6411b3
Skidlez <96402323+korihuen@users.noreply.github.com>
2024-04-08 11:21:05 -0700

    n

--- src/liveFeed.py
+++ src/liveFeed.py
@@ -5,14 +5,21 @@ import math
 
 model = YOLO("yolov8n.pt")
 
-cap = cv2.VideoCapture("rtsp://service:Password!234@192.168.2.3/view.html?mode=l&tcp")
+cap = cv2.VideoCapture("rtsp://service:Password!234@192.168.1.123/view.html?mode=l&tcp")
 
 annotate = sv.BoxAnnotator()
 
-classNames = ["person", "backpack", "umbrella",
-              "handbag", "suitcase", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-              "fork", "knife", "laptop", "cell phone",
-              "teddy bear"]
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
 
 while True:
     success, img = cap.read()
